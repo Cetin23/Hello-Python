83b9c006e7c8c1314d99bd65a5d7460739546bba
Cetin23 <aslantepe.cetin23@gmail.com>
2025-12-11 19:01:35 +0100

    initial Commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+import os #Operatin System -Module
+os.system('clear') # Funktinon- Sytsem call- We pass a argument 
+
+# KOmmand
+'''
+long KOmmend
+'''
+first_name = "John" #String
+age = 37 # Numbers
+names = ["Jony", "Bob", "Mary"] #Lists
+names1 = ("Jony", "Bob", "Mary") #Tuples - du kanns in einer tupple nichts ändern
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob":  "Mushroom",
+	"Mary": "Cheese"
+}#Dictinaries
+
+print (fav_pizza["John"])
